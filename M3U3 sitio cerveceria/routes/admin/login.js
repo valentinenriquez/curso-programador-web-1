74613537aa04f53ad88dc0258b40f07fc382024d
valentin <valenenri99@gmail.com>
2022-06-20 22:47:18 -0300

    M4u4 modificacion

--- M3U3 sitio cerveceria/routes/admin/login.js
+++ M3U3 sitio cerveceria/routes/admin/login.js
@@ -1,12 +1,47 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var usuariosModel = require('./../../models/usuariosModel');
 
-/* GET home page. */
+/* diseno del login */
 router.get('/', function (req, res, next) {
   res.render('admin/login',{
-  layout:'admin/layout'
+    layout:'admin/layout'
   });
 });
+
+//logout//
+router.get('/logout', function (req, res, next) {
+  req.session.destroy(); //destruir
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
+});
+
+
+router.post('/', async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario;
+    var password = req.body.password;
+
+    console.log(req.body);
+
+    var data = await usuariosModel.getUserAndPassword(usuario, password);
+
+    if (data !=undefined) {
+
+        req.session.id_usuario = data.id;
+        req.session.nombre = data.usuario;
+
+      res.redirect('/admin/novedades');
+  } else {
+    res.render('/admin/login', {
+      layout: 'admin/layout',
+      Error: true
+    })
+  } // cierre else
+  } catch (error) {
+    console.log(error)
+  }
+}); // cierra el post
 
 module.exports = router;
